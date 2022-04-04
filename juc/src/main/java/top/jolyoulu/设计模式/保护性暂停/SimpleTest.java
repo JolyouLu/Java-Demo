c2ea6570c151b533fe146b38ea16e7f202573e1b
JolyouLu <1307633126@qq.com>
2022-04-04 16:23:16 +0800

    保护性暂停

--- juc/src/main/java/top/jolyoulu/设计模式/保护性暂停/SimpleTest.java
+++ juc/src/main/java/top/jolyoulu/设计模式/保护性暂停/SimpleTest.java
@@ -62,17 +62,18 @@ class GuardedObject<T> {
 
             //还没有结果
             while (response == null){
+                //计算这次wait需要多久时间，timeout减上一次经历的时间
+                long waitTime = timeout - passedTime;
                 //判断是否已经超时
-                if (passedTime >= timeout){
+                if (waitTime <= 0){
                     break;
                 }
                 try {
-                    //计算需要wait多久时间，timeout减上一次经历的时间
-                    this.wait(timeout - passedTime);
+                    this.wait(waitTime);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                //记录这次wait共记录多久
+                //记录这次wait共用了多久
                 passedTime = System.currentTimeMillis() - begin;
             }
             return response;
