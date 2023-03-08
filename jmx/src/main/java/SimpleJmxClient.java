f04d57759bc07577f8ebfc1cc09840a16a0d9626
luzhoujin <1307633126@qq.com>
2023-03-08 10:50:46 +0800

    jmx 认证登录

--- jmx/src/main/java/SimpleJmxClient.java
+++ jmx/src/main/java/SimpleJmxClient.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
 import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @Author: JolyouLu
@@ -17,18 +20,23 @@ import java.net.MalformedURLException;
  * @Description
  */
 public class SimpleJmxClient {
+    private static final String JMX_USERNAME = "admin";
+    private static final String JMX_PASSWORD = "password";
+
     public static void main(String[] args) {
         String jmxUrl = "service:jmx:rmi:///jndi/rmi://localhost:9999/jmxrmi";
         try {
             JMXServiceURL jmxServiceURL = new JMXServiceURL(jmxUrl);
-            JMXConnector connector = JMXConnectorFactory.newJMXConnector(jmxServiceURL, null);
+            Map<String, Object> env = new HashMap<>();
+            env.put(JMXConnector.CREDENTIALS, new String[] { JMX_USERNAME, JMX_PASSWORD });
+            JMXConnector connector = JMXConnectorFactory.newJMXConnector(jmxServiceURL, env);
             connector.connect();
             MBeanServerConnection msc = connector.getMBeanServerConnection();
             System.out.println("远程服务注册的MBean数量："+msc.getMBeanCount());
-            System.out.println("==========返回任何MBean当前注册的域列表============");
-            String[] domains = msc.getDomains();
-            for (String domain : domains) {
-                System.out.println(domain);
+            System.out.println("==========返回所有MBean当前注册的域列表============");
+            Set<ObjectName> queryMBeans = msc.queryNames(null, null);
+            for (ObjectName objectName : queryMBeans) {
+                System.out.println(objectName);
             }
             System.out.println("==========MBean: com.sun.management============");
             ObjectName objectName = ObjectName.getInstance("JL:type=customer");
--- jmx/src/main/java/SimpleJmxServer.java
+++ jmx/src/main/java/SimpleJmxServer.java
@@ -1,13 +1,14 @@
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
-import javax.management.remote.JMXConnectorServer;
-import javax.management.remote.JMXConnectorServerFactory;
-import javax.management.remote.JMXServiceURL;
+import javax.management.remote.*;
+import javax.security.auth.Subject;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * @Author: JolyouLu
@@ -15,6 +16,9 @@ import java.rmi.registry.LocateRegistry;
  * @Description
  */
 public class SimpleJmxServer {
+    private static final String JMX_USERNAME = "admin";
+    private static final String JMX_PASSWORD = "password";
+
     public static void main(String[] args) throws Exception {
         MBeanServer server = ManagementFactory.getPlatformMBeanServer();
         ObjectName objectName = new ObjectName("JL:type=customer");
@@ -24,7 +28,25 @@ public class SimpleJmxServer {
             LocateRegistry.createRegistry(9999);
             //URL路径的结尾可以随意指定，但如果需要用Jconsole来进行连接，则必须使用jmxrmi
             JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:9999/jmxrmi");
-            JMXConnectorServer jcs = JMXConnectorServerFactory.newJMXConnectorServer(url, null, server);
+            //添加认证步骤
+            HashMap<String, Object> env = new HashMap<>();
+            env.put(JMXConnectorServer.AUTHENTICATOR, new JMXAuthenticator() {
+                @Override
+                public Subject authenticate(Object credentials) {
+                    if (credentials instanceof String[]){
+                        String[] creds = (String[]) credentials;
+                        if (creds.length == 2 && JMX_USERNAME.equals(creds[0]) && JMX_PASSWORD.equals(creds[1])) {
+                            return new Subject(true,
+                                    Collections.singleton(new JMXPrincipal(JMX_USERNAME)),
+                                    Collections.emptySet(),
+                                    Collections.emptySet()
+                            );
+                        }
+                    }
+                    throw new SecurityException("Invalid credentials");
+                }
+            });
+            JMXConnectorServer jcs = JMXConnectorServerFactory.newJMXConnectorServer(url, env, server);
             System.out.println("begin rmi start");
             jcs.start();
             System.out.println("rmi start");
