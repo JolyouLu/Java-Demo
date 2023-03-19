f137682d1184fe187cb0e850f3b7791edc48e22e
Jolyoulu <1307633126@qq.com>
2023-03-19 23:01:59 +0800

    myabtis 插件开发

--- mybatis/src/main/java/top/jolyoulu/mybatis/utils/StatementHandlerMetaObject.java
+++ mybatis/src/main/java/top/jolyoulu/mybatis/utils/StatementHandlerMetaObject.java
@@ -1,11 +1,13 @@
 package top.jolyoulu.mybatis.utils;
 
+import lombok.Data;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 
+@Data
 public class StatementHandlerMetaObject {
 
     private final MetaObject metaObject;
@@ -15,14 +17,33 @@ public class StatementHandlerMetaObject {
     }
 
     public MappedStatement getMappedStatement() {
-        return (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        if (isProxy()){
+            return (MappedStatement) metaObject.getValue("h.target.delegate.mappedStatement");
+        }else {
+            return (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        }
     }
 
     public ParameterHandler getParameterHandler() {
-        return (ParameterHandler) metaObject.getValue("delegate.parameterHandler");
+        if (isProxy()){
+            return (ParameterHandler) metaObject.getValue("h.target.delegate.parameterHandler");
+        }else {
+            return (ParameterHandler) metaObject.getValue("delegate.parameterHandler");
+        }
     }
 
     public void setBoundSql(String sql) {
-        metaObject.setValue("delegate.boundSql.sql",sql);
+        if (isProxy()){
+            metaObject.setValue("h.target.delegate.boundSql.sql",sql);
+        }else {
+            metaObject.setValue("delegate.boundSql.sql",sql);
+        }
+    }
+
+    /**
+     * 是否是代理对象
+     */
+    public boolean isProxy() {
+        return metaObject.hasGetter("h.target");
     }
 }
